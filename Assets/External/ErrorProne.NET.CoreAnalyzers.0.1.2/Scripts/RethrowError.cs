68381e93a559e254d560e75baf0491471e13a04e
bayushi-yakusoku <bayushi.tonio@gmail.com>
2025-01-26 16:45:25 +0100

    del unused "using namespace"

--- Assets/External/ErrorProne.NET.CoreAnalyzers.0.1.2/Scripts/RethrowError.cs
+++ Assets/External/ErrorProne.NET.CoreAnalyzers.0.1.2/Scripts/RethrowError.cs
@@ -1,6 +1,12 @@
 using System;
 using UnityEngine;
 
+
+/*
+ * Part of using Roslyn analyzers.
+ * This class should raise a warning during compilation:
+ * https://docs.unity3d.com/6000.0/Documentation/Manual/install-existing-analyzer.html
+ */
 public class RethrowError : MonoBehaviour
 {
     void Update()
