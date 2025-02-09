50c0c9932280c5456ad8178455714f4c2e90a6d9
bayushi-yakusoku <bayushi.tonio@gmail.com>
2025-02-09 23:49:56 +0100

    test with collision on random objects

--- Assets/ALO/VolleyBall/Scripts/CustomBouncingBall.cs
+++ Assets/ALO/VolleyBall/Scripts/CustomBouncingBall.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomBouncingBall : MonoBehaviour {
@@ -6,18 +7,16 @@ public class CustomBouncingBall : MonoBehaviour {
     float initialVerticalVelocity = 10f; // Initial upward velocity
     float initialHorizontalVelocity = 1f; // Initial horizontal velocity
     
-    [SerializeField] Transform groundPlane; // Reference to the ground plane
-
     private Vector3 velocity;
-    private bool isGrounded = false;
 
-    private Rigidbody rigidbody;
+    // for debug:
+    List<(Vector3, Vector3)> collisionPoints;
 
     void Start() {
         // Apply initial velocities
         velocity = new Vector3(initialHorizontalVelocity, initialVerticalVelocity, 0);
 
-        rigidbody = GetComponent<Rigidbody>();
+        collisionPoints = new ();
     }
 
     void Update() {
@@ -27,55 +26,34 @@ public class CustomBouncingBall : MonoBehaviour {
         // Move the ball
         transform.Translate(velocity * Time.deltaTime);
 
-        //// Check for collision with the ground plane
-        //if (IsGrounded() && !isGrounded) {
-        //    isGrounded = true;
-        //    Bounce();
-        //}
-        //else if (!IsGrounded()) {
-        //    isGrounded = false;
-        //}
+        // Debug-draw all contact points and normals
+        foreach (var collisionPoint in collisionPoints) {
+            Debug.DrawRay(collisionPoint.Item1, collisionPoint.Item2, Color.white);
+        }
+
     }
 
     void OnCollisionEnter(Collision collision) {
-        // Check for collision with any object
-        if (collision.gameObject.CompareTag("BounceObject")) {
-            Bounce(collision.contacts[0].normal);
-        }
+        Debug.Log(this + "reacting to Collision");
+
+        //// Debug-draw all contact points and normals
+        //foreach (ContactPoint contact in collision.contacts) {
+        //    Debug.DrawRay(contact.point, contact.normal, Color.white);
+        //}
+
+        collisionPoints.Add((collision.contacts[0].point, collision.contacts[0].normal));
+
+        // Bounce on the first object:
+        Bounce(collision.contacts[0].normal);
     }
 
-    bool IsGrounded() {
-        // Check if the ball is below or at the ground plane
-        return Vector3.Dot(groundPlane.up, (transform.position - groundPlane.position)) <= 0.5f;
-    }
-
-    void Bounce() {
-        // Calculate the normal of the ground plane
-        Vector3 planeNormal = groundPlane.up;
-
+    void Bounce(Vector3 collisionNormal) {
         // Reflect the velocity off the plane normal
-        velocity = Vector3.Reflect(velocity, planeNormal) * bounceForce;
+        velocity = Vector3.Reflect(velocity, collisionNormal) * bounceForce;
 
         // Optionally, reduce horizontal velocity on bounce to simulate friction
         Vector3 horizontalVelocity = Vector3.ProjectOnPlane(velocity, Vector3.up);
         velocity = horizontalVelocity * 0.9f + Vector3.up * velocity.y;
     }
 
-    void Bounce(Vector3 collisionNormal) {
-        // Reflect the velocity off the collision normal
-        Vector3 reflectedVelocity = Vector3.Reflect(rigidbody.velocity, collisionNormal);
-
-        // Apply the bounce force
-        rigidbody.velocity = reflectedVelocity.normalized * bounceForce;
-    }
-
-    void OnDrawGizmos() {
-        // Draw a line to visualize the ground plane
-        if (groundPlane != null) {
-            Gizmos.color = Color.red;
-            Vector3 planeSize = new Vector3(10, 0, 10);
-            Vector3 planeCenter = groundPlane.position;
-            Gizmos.DrawWireCube(planeCenter, planeSize);
-        }
-    }
 }
